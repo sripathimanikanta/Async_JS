69d8d5ba5688c2a1edb7f2f5d5cde80633858e63
sripathimanikanta <56789000+sripathimanikanta@users.noreply.github.com>
2024-02-16 07:49:59 +0530

    how to parse the httprequest from string to JSON object using remote API endpoint

---
+++ Async_js/http_6.js
@@ -0,0 +1,29 @@
+// parsing into JSON with parse method
+
+const getTodos = (callback) => {
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+    if(request.readyState === 4 && request.status === 200){
+        callback(undefined, request.responseText);
+    }
+    else if(request.readyState === 4 ){
+        callback("Could not fetch data", undefined);
+    }
+})
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send()
+}
+
+getTodos((err, data) => {
+    console.log("callback fired")
+    if(err){
+        console.log(err)
+    }
+    else {
+        console.log(data)
+        const text = JSON.parse(data);
+        console.log(text[0])
+    }
+});
